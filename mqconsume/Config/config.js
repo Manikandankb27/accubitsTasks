87a152127199cbbf4acf2f59796d82ebf33e3d4a
backend <backend@villagecodefactory.com>
2021-06-26 22:50:13 +0530

    config updated

--- mqconsume/Config/config.js
+++ mqconsume/Config/config.js
@@ -1,5 +1,5 @@
 export const baseUrl = 'http://localhost:3000';
-export const mongoURI = 'mongodb://127.0.0.1:27017/stylisthomes';
+export const mongoURI = 'mongodb://127.0.0.1:27017/demo';
 export const nodemailerUser = '';
 export const nodemailerPass = '';
 export const connURL = 'amqp://localhost';
--- queue/Config/config.js
+++ queue/Config/config.js
@@ -1,3 +1,3 @@
 export const baseUrl = 'http://localhost:3000';
-export const mongoURI = 'mongodb://127.0.0.1:27017/stylisthomes';
+export const mongoURI = 'mongodb://127.0.0.1:27017/demo';
 export const connURL = 'amqp://localhost';
